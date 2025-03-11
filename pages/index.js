69d9098d0949a91d6a1488b37b38ca98a5e4ae86
Lucca Kazlauskas Zaccaria <zaccaria.kaz@gmail.com>
2025-03-11 20:23:09 -0300

    alterando mensagem do site para a versão mãe

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 function Home() {
-    return <h1> Cinthia, te amo. Tenho certeza que vamos conquistar o mundo juntos! Se você topar, da uma risadinha pra mim </h1>
+    // return <h1> Cinthia, te amo. Tenho certeza que vamos conquistar o mundo juntos! Se você topar, da uma risadinha pra mim </h1>
+    return <h1> Oi mamis da um sorriso </h1>
 }
 
 export default Home
