873771c1dc88b21717f887285e5019914aabf552
Lucca Kazlauskas Zaccaria <zaccaria.kaz@gmail.com>
2025-03-21 14:56:05 -0300

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
 function Home() {
-    return <h1> Cinthia, te amo. Tenho certeza que vamos conquistar o mundo juntos! Se você topar, as passagens estão compradas. Bora ? </h1>
+  return (
+    <h1>
+      Cinthia, te amo. Tenho certeza que vamos conquistar o mundo juntos! Se
+      você topar, as passagens estão compradas. Bora ?
+    </h1>
+  );
 }
 
-export default Home
+export default Home;
