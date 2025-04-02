45c1f7623afed1744771caefab4674959b1b1acb
Lucca Kazlauskas Zaccaria <zaccaria.kaz@gmail.com>
2025-04-02 18:58:47 -0300

    add `database.js` and `.env` files

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,7 +1,9 @@
-function status(request, response) {
-  response
-    .status(200)
-    .json({ mensagem: "ta dando certo esse carai", chave: 200 });
+import database from "../../../../infra/database.js";
+
+async function status(request, response) {
+  const result = await database.query("SELECT 1 + 1 as sum;");
+  console.log(result.rows);
+  response.status(200).json({ mensagem: "ta dando certo", chave: 200 });
 }
 
 export default status;
