d4a0422a724cdfbad9ca3c93af2b0fc659a426ff
Lucca Kazlauskas Zaccaria <zaccaria.kaz@gmail.com>
2025-04-14 15:22:11 -0300

    fix some bugs in connection with db

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -4,8 +4,9 @@ import database from "infra/database.js";
 
 export default async function migrations(request, response) {
   const dbClient = await database.getNewClient();
+
   const defaultMigrationsOptions = {
-    databaseUrl: process.env.DATABASE_URL,
+    dbClient: dbClient,
     dryRun: true,
     dir: join("infra", "migrations"),
     direction: "up",
