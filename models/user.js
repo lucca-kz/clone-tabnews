d3f8745e3ec911c5e21fdfef4a27031355af808e
Lucca Kazlauskas Zaccaria <zaccaria.kaz@gmail.com>
2025-10-24 15:32:06 +0200

    feat: create `user.findOneByUsername` method and `api/v1/users/[username]` endpoint

--- models/user.js
+++ models/user.js
@@ -1,5 +1,27 @@
 import database from "infra/database.js";
-import { ValidationError } from "infra/errors.js";
+import { ValidationError, NotFoundError } from "infra/errors.js";
+
+async function findOneByUsername(username) {
+  const userFound = await runSelectQuery(username);
+
+  return userFound;
+
+  async function runSelectQuery(username) {
+    const results = await database.query({
+      text: `SELECT * FROM users WHERE LOWER(username) = LOWER($1) LIMIT 1;`,
+      values: [username],
+    });
+
+    if (results.rowCount === 0) {
+      throw new NotFoundError({
+        message: "O username informado não foi encontrado.",
+        action: "Verifique se o username foi digitado corretamente.",
+      });
+    }
+
+    return results.rows[0];
+  }
+}
 
 async function create(userInputValues) {
   await validateUniqueEmail(userInputValues.email);
@@ -53,6 +75,7 @@ async function create(userInputValues) {
 
 const user = {
   create,
+  findOneByUsername,
 };
 
 export default user;
